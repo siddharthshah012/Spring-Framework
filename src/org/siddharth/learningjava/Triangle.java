56c1e05d41781c1954309dcf5ec21e58a2bad059
siddharthshah012 <sshah8@mail.csuchico.edu>
2017-03-07 02:13:30 -0800

    Constructor Injection

--- src/org/siddharth/learningjava/Triangle.java
+++ src/org/siddharth/learningjava/Triangle.java
@@ -3,16 +3,36 @@ package org.siddharth.learningjava;
 public class Triangle {
 	
 	private String type;
+	private int height;
 	
-	public String getType() {
-		return type;
+	
+	
+	public int getHeight() {
+		return height;
 	}
-	public void setType(String type) {
+	/***** Constructor for type*****/
+	public Triangle (String type){
 		this.type = type;
 	}
 	
+	/***** Constructor for height*****/
+	public Triangle (int height){
+		this.height = height;
+	}
+	/***** Constructor for type and height*****/
+	public Triangle (String type, int height){
+		this.type = type;
+		this.height = height;
+	}
+	public String getType() {
+		return type;
+	}
+	/*public void setType(String type) {
+		this.type = type;
+	}*/
+	
 	public void draw(){
-		System.out.println(getType() + " Triangle is Drawn");
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
 	}
 
 }
